f86035e61d6408d690f8c7a3a13c1863927c22ce
chandra271193 <161210448+chandra271193@users.noreply.github.com>
2024-03-03 15:47:42 +0530

    Add files via upload
    Week1-Day1 Assignments

---
+++ day1/FindNumber.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class FindNumber {
+
+	public static void main(String[] args) {
+		int a=45;
+		if(a>0)
+		System.out.println("Given Number a="  +a+  "is Positive");
+		else
+			System.out.println("Given Number a=" +a+ "is negative");
+			
+		}
+
+	}
+
+
---
+++ day1/browser.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class browser {
+
+
+public static void main(String[] args) {
+		float browserVersion= 1.0f;
+		String browserName= "Chrome";
+		boolean pageLoaded=true;
+		int noOfBrowser=3;
+		char browserLogo='c';
+		System.out.println("Browser Name:" +browserName+"\n"+"Browser Version:"+browserVersion+"\n"+"Is Loaded:"+pageLoaded+"\n"+"No Of Browsers:"+noOfBrowser+"\n"+"Browser Logo:"+browserLogo);
+
+
+	}
+
+}
